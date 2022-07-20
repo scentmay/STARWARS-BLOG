bc6e480ea3e8cf6d0d9fd991b375d36862c338a5
Facundopiedrabuena <faculpda46@gmail.com>
2022-07-20 15:49:46 +0000

    facundo

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,18 @@
+
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Personaje } from '../views/personaje.jsx';
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="text-center mt-5 d-flex overflow-scroll" style={{overflow: 'auto', whiteSpace: 'nowrap'}}>
+		<Personaje />
+		<Personaje />
+		<Personaje />
+		<Personaje />
+		<Personaje />
+		<Personaje />
+		<Personaje />
+		<Personaje />
 	</div>
 );
